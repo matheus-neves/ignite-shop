57e75ee34a878e248c7cff13e0c2d430b1faaef0
Matheus Neves <matheusneves1989@gmail.com>
2023-02-20 19:51:19 -0300

    feat(pages/home): implement home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,53 @@
+import { HomeContainer, Product } from "@/styles/pages/home"
+import Image from "next/image"
+import { useKeenSlider } from 'keen-slider/react'
+
+import tshirt1 from '../assets/temp/1.png';
+import tshirt2 from '../assets/temp/2.png';
+import tshirt3 from '../assets/temp/3.png';
+
+import 'keen-slider/keen-slider.min.css';
+
+
 export default function Home() {
+
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  });
+
   return ( 
-    <h1>Hello world!</h1>
+    <HomeContainer ref={sliderRef} className='keen-slider'>
+      <Product className='keen-slider__slide'>
+        <Image src={tshirt1} alt='' width={520} height={480} />
+        <footer>
+          <strong>T-shirt X</strong>
+          <span>$79.90</span>
+        </footer>
+      </Product>
+      <Product className='keen-slider__slide'>
+        <Image src={tshirt2} alt='' width={520} height={480} />
+        <footer>
+          <strong>T-shirt X</strong>
+          <span>$79.90</span>
+        </footer>
+      </Product>
+      <Product className='keen-slider__slide'>
+        <Image src={tshirt3} alt='' width={520} height={480} />
+        <footer>
+          <strong>T-shirt X</strong>
+          <span>$79.90</span>
+        </footer>
+      </Product>
+      <Product className='keen-slider__slide'>
+        <Image src={tshirt3} alt='' width={520} height={480} />
+        <footer>
+          <strong>T-shirt X</strong>
+          <span>$79.90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
